baf675936b78b246449574622001329ddf7bada4
abelfentaw <abelfentaw2000@gmail.com>
2025-02-03 18:25:17 +0300

    fix automatic refresh

--- backend/server.js
+++ backend/server.js
@@ -17,13 +17,6 @@ app.use(express.json());
 const allowedOrigins = ["http://localhost:5173", "http://localhost:5174"];
 
 app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true
 }));
